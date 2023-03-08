9b4a72e0ab91fdb2cc3b8ac96d970515a2999f10
ijosue16 <iradukundajosue16@gmail.com>
2023-03-08 16:19:12 +0200

    add decode method

---
+++ morse_code_decoder.rb
@@ -0,0 +1,42 @@
+def decode_char(encoded_char)
+    morse_characters = {
+        "a" => ".-",
+        "b" => "-...",
+        "c" => "-.-.",
+        "d" => "-..",
+        "e" => ".",
+        "f" => "..-.",
+        "g" => "--.",
+        "h" => "....",
+        "i" => "..",
+        "j" => ".---",
+        "k" => "-.-",
+        "l" => ".-..",
+        "m" => "--",
+        "n" => "-.",
+        "o" => "---",
+        "p" => ".--.",
+        "q" => "--.-",
+        "r" => ".-.",
+        "s" => "...",
+        "t" => "-",
+        "u" => "..-",
+        "v" => "...-",
+        "w" => ".--",
+        "x" => "-..-",
+        "y" => "-.--",
+        "z" => "--.."
+    }
+    morse_characters.each do|key, value|
+    return key.upcase if encoded_char == value
+end
+end
+
+def decode_word(encoded_word)
+    letters=encoded_word.split(' ')
+    word=''
+    letters.each do |char|
+    word +=decode_char(char)
+    end
+    return word
+end
