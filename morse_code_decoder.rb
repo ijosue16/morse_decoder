162f3f2156ca6088f46d9b106817666cce8f255e
felixDev22 <felixnzomo@gmail.com>
2023-03-08 17:46:30 +0300

    Add funtion for decode message

--- morse_code_decoder.rb
+++ morse_code_decoder.rb
@@ -1,42 +1,52 @@
 def decode_char(encoded_char)
-    morse_characters = {
-        "a" => ".-",
-        "b" => "-...",
-        "c" => "-.-.",
-        "d" => "-..",
-        "e" => ".",
-        "f" => "..-.",
-        "g" => "--.",
-        "h" => "....",
-        "i" => "..",
-        "j" => ".---",
-        "k" => "-.-",
-        "l" => ".-..",
-        "m" => "--",
-        "n" => "-.",
-        "o" => "---",
-        "p" => ".--.",
-        "q" => "--.-",
-        "r" => ".-.",
-        "s" => "...",
-        "t" => "-",
-        "u" => "..-",
-        "v" => "...-",
-        "w" => ".--",
-        "x" => "-..-",
-        "y" => "-.--",
-        "z" => "--.."
-    }
-    morse_characters.each do|key, value|
+  morse_characters = {
+    "a" => ".-",
+    "b" => "-...",
+    "c" => "-.-.",
+    "d" => "-..",
+    "e" => ".",
+    "f" => "..-.",
+    "g" => "--.",
+    "h" => "....",
+    "i" => "..",
+    "j" => ".---",
+    "k" => "-.-",
+    "l" => ".-..",
+    "m" => "--",
+    "n" => "-.",
+    "o" => "---",
+    "p" => ".--.",
+    "q" => "--.-",
+    "r" => ".-.",
+    "s" => "...",
+    "t" => "-",
+    "u" => "..-",
+    "v" => "...-",
+    "w" => ".--",
+    "x" => "-..-",
+    "y" => "-.--",
+    "z" => "--.."
+  }
+  morse_characters.each do |key, value|
     return key.upcase if encoded_char == value
-end
+  end
 end
 
 def decode_word(encoded_word)
-    letters=encoded_word.split(' ')
-    word=''
-    letters.each do |char|
-    word +=decode_char(char)
-    end
-    return word
+  letters = encoded_word.split(' ')
+  word = ''
+  letters.each do |char|
+    word += decode_char(char)
+  end
+  return word
 end
+
+def decode_message(message)
+  sentence = message.split('   ')
+  answer = ''
+  sentence.each do |word|
+    answer += decode_word(word).concat(' ')
+  end
+  return answer
+end
+puts decode_message(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
